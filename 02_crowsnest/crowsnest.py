d12322598ad4c31cbc52e3d15e9bf12286388f2d
Ayun Daywhea <32340123+citypop56709@users.noreply.github.com>
2022-04-27 18:23:20 -0500

    Chapter 2.4 extra challenges complete.

--- 02_crowsnest/crowsnest.py
+++ 02_crowsnest/crowsnest.py
@@ -5,7 +5,10 @@ Date   : 2022-04-23
 Purpose: Alert the crew!
 """
 
+# flake8: noqa
 import argparse
+import string
+from curses.ascii import isdigit
 
 
 # --------------------------------------------------
@@ -17,11 +20,14 @@ def get_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument(
-        'thing',
-        metavar='str',
+        'word',
+        metavar='word',
         type=str,
         help='The creature that the sailor will call out to the crew.')
-
+    parser.add_argument(
+        '--starboard',
+        action='store_true',
+        help='The point of the bow the creature is coming from')
     return parser.parse_args()
 
 
@@ -29,12 +35,22 @@ def get_args():
 def main():
     """Make a jazz noise here"""
 
-    vowels = ['a', 'e', 'i', 'o', 'u']
+    vowels = 'aeiou'
     args = get_args()
-    article = 'a'
-    if (args.thing[0].lower() in vowels):
-        article = 'an'
-    print(f'Ahoy, Captain, {article} {args.thing} off the larboard bow!')
+    for character in args.word:
+        if (character in string.punctuation or isdigit(character)):
+            print(
+                f'{args.word} is invalid. Input cannot contain numbers or special characters.'  #noqa
+            )
+            return
+    char = args.word[0]
+    direction = 'larboard'
+    article = 'an' if char.lower() in vowels else 'a'
+    if (char.isupper()):
+        article = article.capitalize()
+    if (args.starboard):
+        direction = 'starboard'
+    print(f'Ahoy, Captain, {article} {args.word} off the {direction} bow!')
 
 
 # --------------------------------------------------
--- 02_crowsnest/test.py
+++ 02_crowsnest/test.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """tests for crowsnest.py"""
 
+import random
+import string
 import os
+from sre_parse import SPECIAL_CHARS
 from subprocess import getstatusoutput, getoutput
+# flake8: noqa
 
 prg = './crowsnest.py'
 consonant_words = [
@@ -12,7 +16,9 @@ consonant_words = [
     'zebrafish'
 ]
 vowel_words = ['aviso', 'eel', 'iceberg', 'octopus', 'upbound']
+digit = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 template = 'Ahoy, Captain, {} {} off the larboard bow!'
+errorTemplate = '{} is invalid. Input cannot contain numbers or special characters.'
 
 
 # --------------------------------------------------
@@ -42,12 +48,16 @@ def test_consonant():
 
 
 # --------------------------------------------------
+    """Function is changed so that it will check to make sure that the article is in uppercase as well. #noqa
+    """
+
+
 def test_consonant_upper():
     """brigantine -> a Brigatine"""
 
     for word in consonant_words:
         out = getoutput(f'{prg} {word.title()}')
-        assert out.strip() == template.format('a', word.title())
+        assert out.strip() == template.format('A', word.title())
 
 
 # --------------------------------------------------
@@ -60,9 +70,47 @@ def test_vowel():
 
 
 # --------------------------------------------------
+    """Function is changed so that it will check to make sure that the article is in uppercase as well. #noqa
+    """
+
+
 def test_vowel_upper():
     """octopus -> an Octopus"""
 
     for word in vowel_words:
         out = getoutput(f'{prg} {word.upper()}')
-        assert out.strip() == template.format('an', word.upper())
+        assert out.strip() == template.format('An', word.upper())
+
+
+# --------------------------------------------------
+    """The function will check to see if program will generate an error message if there is a number in the word. #noqa
+    """
+
+
+def test_number():
+    """octopus -> 1Octopus"""
+
+    for word in consonant_words:
+        random_index = random.randint(0, len(word) - 1)
+        word_random_letter = word[random_index]
+        digit = random.choice(string.digits)
+        word = word.replace(word_random_letter, digit, 1)
+        out = getoutput(f'{prg} {word}')
+        assert out.strip() == errorTemplate.format(word)
+
+
+# --------------------------------------------------
+    """The function will check to see if program will generate an error message if there is a special character in the word. #noqa
+    """
+
+
+def test_special_char():
+    """octopus -> %Octopus"""
+
+    for word in consonant_words:
+        random_index = random.randint(0, len(word) - 1)
+        word_random_letter = word[random_index]
+        special_char = random.choice(string.punctuation)
+        word = word.replace(word_random_letter, special_char, 1)
+        out = getoutput(f'{prg} \'{word}\'')
+        assert out.strip() == errorTemplate.format(word)
